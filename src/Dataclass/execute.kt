55e5ca4521cc93b9705a822dbffa2c0c2e72086e
willshuffy <saputrawilly26@gmail.com>
2021-03-04 12:48:36 +0700

    copy data class

--- src/Dataclass/execute.kt
+++ src/Dataclass/execute.kt
@@ -8,5 +8,19 @@ fun main() {
 
     println(hero)
 
+    //==================================================================================================================
+    /*
+        copy data class
+            - data class memiliki function copy yang bisa digunakan untuk menduplikasi object
+            - bahkan metode copy tsb bisa sekaligus mengubah propertiesnya
+     */
 
+    val hero2 = hero.copy()
+    println(hero2)
+
+    val hero3 = hero.copy(name = "Robin")
+    println(hero3)
+
+    val hero4 = hero.copy(name = "Superman", action = "Flight")
+    println(hero4)
 }
